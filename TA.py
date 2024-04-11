6df99a619fcf6fb5c071c6dd1c7b1bec8c1c905d
999sanfentang <shyyna@163.com>
2024-04-11 22:39:19 +0800

    main

--- TA.py
+++ TA.py
@@ -75,8 +75,6 @@ class TripletAttention(nn.Module):
         x_out2 = self.hc(x_perm2)
         # b,w,h,c => b,c,h,w
         x_out21 = x_out2.permute(0, 3, 2, 1).contiguous()
-        
-        
         if not self.no_spatial:
             x_out = self.hw(x)
             x_out = 1 / 3 * (x_out + x_out11 + x_out21)
